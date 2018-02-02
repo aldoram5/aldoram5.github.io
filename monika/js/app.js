2ea29b9a3cfda37d39bffccd0792bf9904eb1ac1
aldoram5 <master-sword5al@hotmail.com>
2018-02-01 22:18:07 -0600

    Export chat button worksgit add monika/

--- monika/js/app.js
+++ monika/js/app.js
@@ -4,4 +4,8 @@
  * Copyright 2018 Aldo Pedro Rangel Montiel (aldoram5)
  */
 
- angular.module('MonikaChatEditorApp',['ngMaterial']);
+angular.module('MonikaChatEditorApp', ['ngMaterial'])
+        .config(function ($compileProvider) {
+            $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|tel|file|blob):/);
+
+        });
--- monika/js/chatEditor.js
+++ monika/js/chatEditor.js
@@ -7,7 +7,7 @@
 angular.module('MonikaChatEditorApp')
         .controller('EditorCtrl', editorCtrl);
 
-editorCtrl.$inject = ['$scope','$mdDialog'];
+editorCtrl.$inject = ['$scope', '$mdDialog'];
 
 function editorCtrl($scope, $mdDialog) {
 
@@ -19,62 +19,77 @@ function editorCtrl($scope, $mdDialog) {
         nextNode: "",
         nodes: []
     };
-    
+    $scope.coversationName = "chat";
     $scope.currentId = 0;
-    $scope.actionTypes = ["check","store"];
-    $scope.nodeInputTypes = ["multi","text"];
-    
-    $scope.addNode = function(event){
+    $scope.actionTypes = ["check", "store"];
+    $scope.nodeInputTypes = ["multi", "text"];
+    $scope.url = null;
+
+    $scope.addNode = function (event) {
         $scope.currentId++;
         var node = {
-            id : "N"+$scope.currentId,
-            initialAction : null,
+            id: "N" + $scope.currentId,
+            initialAction: null,
             finalAction: null,
             inputType: null,
             nextNode: "",
-            reaction : "1a",
-            display_text:"",
+            reaction: "1a",
+            display_text: "",
             options: []
-            
+
         };
         $scope.conversation.nodes.push(node);
-        if ($scope.conversation.nodes.length == 1){
+        if ($scope.conversation.nodes.length == 1) {
             $scope.conversation.nextNode = node.id;
         }
     };
-    $scope.createAction = function(){
-      return {type:"",key:"",value:""};  
+
+    $scope.exportJSON = function (event) {
+        if ($scope.url != null) {
+            $scope.url.revokeObjectURL();
+        }
+        var jsonToExport = angular.toJson($scope.conversation);
+        var blob = new Blob([jsonToExport], {type: 'text/plain'});
+        $scope.url = (window.URL || window.webkitURL).createObjectURL(blob);
+        var exportLink = angular.element('<a></a>');
+        exportLink.attr('href', $scope.url);
+        exportLink.attr('download', 'monika-'+$scope.coversationName +'.json');
+        exportLink[0].click();
     };
-    
-    $scope.addFinalActionToNode = function(event, node){
+
+    $scope.createAction = function () {
+        return {type: "", key: "", value: ""};
+    };
+
+    $scope.addFinalActionToNode = function (event, node) {
         node.finalAction = $scope.createAction();
-        
+
     };
-    
-    $scope.addInitialActionToNode = function(event, node){
+
+    $scope.addInitialActionToNode = function (event, node) {
         node.initialAction = $scope.createAction();
-        
+
     };
-    $scope.removeFinalActionToNode = function(event, node){
+    $scope.removeFinalActionToNode = function (event, node) {
         node.finalAction = null;
-        
+
     };
-    
-    $scope.removeInitialActionToNode = function(event, node){
+
+    $scope.removeInitialActionToNode = function (event, node) {
         node.initialAction = null;
-        
+
     };
-    
-    $scope.addMenuOptionToNode = function(event, node){
-        node.options.push({"text":"", "nextNode": null});
+
+    $scope.addMenuOptionToNode = function (event, node) {
+        node.options.push({"text": "", "nextNode": null});
     };
-    
-    $scope.removeFinalActionToNode = function(event, node, index){
-        node.options.splice(index,1);
+
+    $scope.removeFinalActionToNode = function (event, node, index) {
+        node.options.splice(index, 1);
     };
 
     $scope.init = function () {
-        
+
     };
     $scope.init();
 }
